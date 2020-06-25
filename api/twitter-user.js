454d0744c56e45c725c231592ed619e1d78f56ea
César Carruitero <ccarruitero@protonmail.com>
2020-06-25 01:06:59 -0500

    use twitter-lite for get token

--- api/twitter-user.js
+++ api/twitter-user.js
@@ -1,21 +1,31 @@
-const http = require('http');
+const Twitter = require('twitter-lite');
+const { get } = require('https');
 
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
-
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   const { name } = req.query;
+
+  const client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET
+  });
+
+  const tokenResponse = await client.getBearerToken();
+
   const url = `https://api.twitter.com/1.1/users/show.json?screen_name=${name}`;
 
   const options = {
     headers: {
-      'Authorization': `Bearer ${process.env.TWITTER_TOKEN}`
+      'Authorization': `Bearer ${tokenResponse.access_token}`
     }
   }
-  http.get(url, options, (response) => {
+
+  get(url, options, (response) => {
     response.on('data', (data) => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
       res.json(JSON.parse(data.toString()))
+    })
+    response.on('error', (err) => {
+      res.send(err);
     })
   });
 }
